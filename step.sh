eb48c740b241cad360ff5ad036dd9e2808ec8fd6
Mone Rohith <monerohith@Admins-MacBook-Pro.local>
2022-03-11 21:05:25 +0530

    Added custom id

--- step.sh
+++ step.sh
@@ -2,6 +2,7 @@
 set -e
 set -x
 set -o pipefail
+
 curl -u "$lambdatest_username:$lambdatest_access_key" --location --request POST https://manual-api.lambdatest.com/app/upload/realDevice --header 'Content-Type: application/json' --data-raw '{"url":"'$upload_path'","custom_id":"'$custom_id'", "name":"samplefile"}' -o ".upload-app-response.json" | jq -j '.app_url' 
 
 APP_URL=$(cat ".upload-app-response.json" | ack -o --match '(?<=app_url\":")([_\%\&=\?\.aA-zZ0-9:/-]*)')
