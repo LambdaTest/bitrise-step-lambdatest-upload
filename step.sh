2b022f0461f8ebbefd502f5b16fcebe87618334b
rohithmone-lt <monerohith@lambdatest.com>
2022-05-05 16:54:14 +0530

    Fixed app upload for local paths

--- step.sh
+++ step.sh
@@ -3,7 +3,24 @@ set -e
 set -x
 set -o pipefail
 
-curl -u "$lambdatest_username:$lambdatest_access_key" --location --request POST https://manual-api.lambdatest.com/app/upload/realDevice --header 'Content-Type: application/json' --data-raw '{"url":"'$upload_path'","custom_id":"'$custom_id'", "name":"'$app_name'"}' -o ".upload-app-response.json" | jq -j '.app_url' 
+UPLOAD_PATH=${upload_path}
+APP_NAME=${app_name}
+CUSTOM_ID=${custom_id}
+BASICAUTH=$(echo -n $lambdatest_username:$lambdatest_access_key | base64)
+
+if [ -z "${UPLOAD_PATH##*http*}" ]; then
+    curl -u "$lambdatest_username:$lambdatest_access_key" --location --request POST https://manual-api.lambdatest.com/app/upload/realDevice \
+    --header 'Content-Type: application/json' \
+    --data-raw '{"url":"'$UPLOAD_PATH'","custom_id":"'$custom_id'", "name":"'$APP_NAME'"}' \
+    -o ".upload-app-response.json"
+else
+    curl --location --request POST 'https://manual-api.lambdatest.com/app/upload/realDevice' \
+    --header "Authorization: Basic $BASICAUTH" \
+    --form "name=$APP_NAME" \
+    --form "custom_id=$CUSTOM_ID" \
+    --form "appFile=@"$UPLOAD_PATH"" \
+    -o ".upload-app-response.json"
+fi
 
 APP_URL=$(cat ".upload-app-response.json" | ack -o --match '(?<=app_url\":")([_\%\&=\?\.aA-zZ0-9:/-]*)')
 envman add --key LAMBDATEST_APP_URL --value ${APP_URL}
